11474bd65c7e89d01f075138d0a50399ba7f47ce
Mir Ashif <mirashif01@gmail.com>
2022-09-15 05:48:37 +0600

    conditional text

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -24,44 +24,45 @@ const Home = () => {
     return () => clearInterval(interval);
   }, [setTexts]);
 
-  if (!texts.length) {
-    return (
-      <h1 className="grid h-screen w-full place-items-center text-2xl">
-        Copy texts and they will appear here!
-      </h1>
-    );
-  }
-
   return (
     <div className="flex flex-col">
       <header className="navbar sticky top-0 z-10 flex justify-between bg-base-200 p-4">
         <h1 className="flex-1 text-2xl">Clippy</h1>
-        <button
-          onClick={() => window.electron.deleteTexts()}
-          type="button"
-          className="btn btn-xs flex-none"
-        >
-          Clear all
-        </button>
-      </header>
-      <ul className="flex flex-col gap-3 px-4 py-2">
-        {texts.reverse().map(({ id, text }) => (
+        {texts.length > 0 && (
           <button
-            onClick={() => {
-              window.electron.addText(text);
-            }}
+            onClick={() => window.electron.deleteTexts()}
             type="button"
-            key={id}
-            className="card card-compact basis-full border-base-100 bg-base-100 shadow-sm transition hover:border-base-content hover:shadow-md"
+            className="btn btn-xs flex-none"
           >
-            <div className="flex w-full justify-between p-4">
-              <p className="text-start text-base leading-relaxed line-clamp-3">
-                {text}
-              </p>
-            </div>
+            Clear all
           </button>
-        ))}
-      </ul>
+        )}
+      </header>
+
+      {!texts.length ? (
+        <h2 className="my-6 text-center text-base italic">
+          Copy texts and they will appear here!
+        </h2>
+      ) : (
+        <ul className="flex flex-col gap-3 px-4 py-2">
+          {texts.reverse().map(({ id, text }) => (
+            <button
+              onClick={() => {
+                window.electron.addText(text);
+              }}
+              type="button"
+              key={id}
+              className="card card-compact basis-full border-base-100 bg-base-100 shadow-sm transition hover:border-base-content hover:shadow-md"
+            >
+              <div className="flex w-full justify-between p-4">
+                <p className="text-start text-base leading-relaxed line-clamp-3">
+                  {text}
+                </p>
+              </div>
+            </button>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
